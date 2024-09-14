2b633d36c7d81b60949007d22ce1c3512454eeeb
Luke McNeil <lukemcneil61@gmail.com>
2024-09-14 15:19:41 -0500

    default game room

--- scripts/join_screen.gd
+++ scripts/join_screen.gd
@@ -10,6 +10,8 @@ var game_scene := preload("res://scenes/game.tscn")
 func _on_join_pressed() -> void:
 	var game: Game = game_scene.instantiate()
 	game.game_room = game_room.text
+	if game.game_room.is_empty():
+		game.game_room = "test"
 	game.player = player.selected as Game.Player
 	get_tree().root.add_child(game)
 	get_tree().root.remove_child(self)
